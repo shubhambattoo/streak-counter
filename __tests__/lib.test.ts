e036f7f21647f67bbeda743aab71f4237ae7b73b
shubhambattoo <shubham.battoo@hotmail.com>
2023-03-31 15:37:16 +0530

    updated lib tests

--- __tests__/lib.test.ts
+++ __tests__/lib.test.ts
@@ -4,7 +4,7 @@ import { shouldIncrementOrResetStreakCount } from '../src/lib';
 describe('shouldIncrementOrResetStreakCount', () => {
   it('should return none if same day', () => {
     const input = new Date(
-      'Tue Dec 27 2022 00:00:00 GMT+0530 (India Standard Time)'
+      'Tue Dec 27 2022'
     );
     const output = shouldIncrementOrResetStreakCount(input, '12/27/2022');
     const expected = 'none';
